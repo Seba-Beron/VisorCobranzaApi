2f80a4d5b2dd1ed993625d15dcd31b640edf2a26
Sebastian Beron <43069682+Seba-Beron@users.noreply.github.com>
2024-04-19 12:19:43 -0300

    login working

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -2,16 +2,17 @@
 
 const { response } = require('express');
 const { Pool } = require('pg');
-const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'Carlos17%',
-    database: 'postgres',
-    port: 5432
-})
 const globalConstants = require('../const/globalConstants.js')
 const jwt = require('jsonwebtoken')
 var bcrypt = require('bcryptjs');
+
+const pool = new Pool({
+    host: globalConstants.HOST,
+    user: globalConstants.USER,
+    password: globalConstants.PASSWORD,
+    database: globalConstants.DATABASE,
+    port: globalConstants.PORT
+})
 
 module.exports = {
 
@@ -23,32 +24,37 @@ module.exports = {
             res.json({ message: 'Datos incompletos' });
         }
         else {
-            let passHash = await bcrypt.hash(password, 8);
-            console.log(passHash);
-            let query = `select login('${rut}', '${passHash}')`;
-
             try {
-                const response = await pool.query(query);
-                console.log(response);
+                data = await pool.query(`select * from login('${rut}')`);
+            } catch (e) {
+                res.json({message: e})
+            }
 
+            if (data.rows.length == 0){
+                res.json({ message: 'rut incorrecto' });
+            }
+
+            let user = data.rows[0];
+            let autenticado = await bcrypt.compare(password, user.password);
+
+            if (autenticado) {
+                    
                 var fechaActual = new Date();
                 fechaActual.setHours(fechaActual.getHours() + 1);
 
                 const userForToken = {
-                    rut: response.rut,
-                    username: response.name,
+                    rut: rut,
+                    username: user.name,
                     expirationDate: fechaActual
                 }
 
-                const token = jwt.sign(userForToken, globalConstants.SECRETWORD)
-
                 res.json({
-                    token: token
+                    token: jwt.sign(userForToken, globalConstants.SECRETWORD),
+                    expiratedPassword: user.expirated
                 })
-            } catch (e) {
-                res.json({
-                    message: e
-                })
+            }
+            else{
+                res.json({ message: 'contraseña incorrecta' });
             }
         }
     },
