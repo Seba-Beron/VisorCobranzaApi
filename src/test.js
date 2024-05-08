42a328466f66288f67e367f043f71c412365d551
Sebastian Beron <43069682+Seba-Beron@users.noreply.github.com>
2024-05-08 12:53:05 -0300

    correccion type module

--- src/test.js
+++ src/test.js
@@ -1,5 +1,7 @@
-const { test } = require("node:test");
-const { assert } = require("node:assert");
+import test from 'node:test'
+import assert from 'node:assert'
+//const { test } = require("node:test");
+//const { assert } = require("node:assert");
 
 test('example test', () => {
   assert.equal(1,1)
